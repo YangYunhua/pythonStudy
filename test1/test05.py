9cf6ae9a31940406e721eab17429e874f6ebd3fb
YangYunhua <1332850274@qq.com>
2017-02-24 11:13:25 +0800

    Initial commit

--- test1/test05.py
+++ test1/test05.py
@@ -1,8 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import HTMLTestRunner
 
 __author__ = 'yangyh'
 
-import requests
+import requests,unittest,json
+class Testbaidu(unittest.TestCase):
+    def setUp(self):
+        url="http://fanyi.baidu.com/v2transapi"
 
-url="http"
+    def testzhen(self):
+        #接口参数
+        params = {
+        "from":"en",
+        "to":"zh",
+        "query":"study"
+     }
+        url="http://fanyi.baidu.com/v2transapi"
+        #发送参数
+        r = requests.request("post",url,params=params)
+        r = json.loads(r.text)
+        assert (u'学习'in r['liju_result']['tag'])
+"""
+    def testzhen2(self):
+        params = {
+            "from":"en",
+            "to":"h",
+            "query":"stud"
+        }
+        url="http://fanyi.baidu.com/v2transapi"
+        #发送参数
+        r = requests.request("post",url,params=params)
+        r = json.loads(r.text)
+        assert (u'学'in r['liju_result']['tag'])
+
+     def tearDown(self):
+        pass
+"""
+if __name__ == '__main__':
+    report_dir=r's.html'
+    re_open=open(report_dir,'wb')
+    suite=unittest.TestLoader().loadTestsFromTestCase(Testbaidu)
+    runner=HTMLTestRunner.HTMLTestRunner(
+        stream=re_open,
+        title=u'百度翻译api接口测试',
+        description=u'百度翻译api接口测试详情'
+    )
+    runner.run(suite)
